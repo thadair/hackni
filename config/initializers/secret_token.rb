a492018c01fcc869f35de2e6b5428b4cd5c1a676
thadair <thaddaniel@gmail.com>
2013-11-18 14:03:45 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackni::Application.config.secret_key_base = '31c205fb6f69af68a476227913a8fd71daaa055a96d83e0cd08dc7159a5a82fcfac1f1d63e0ff29d17a6c844c35f3c55c39d7929e79f8d37e3c7e43b24be2734'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hackni::Application.config.session_store :cookie_store, key: '_hackni_session'
